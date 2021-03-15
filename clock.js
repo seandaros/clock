165f127dd2c9cb194168407cd614ba1602592d5e
seandaros <65143921+seandaros@users.noreply.github.com>
2021-03-14 23:10:40 -0400

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,49 +1,188 @@
 function washingtonTimeClock(){
     var washingtonTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York", timeStyle: "medium", hourCycle: "h23"});
-    
-    document.getElementById("washingtonClock").innerText = washingtonTime;
-    document.getElementById("washingtonClock").textContent = washingtonTime;
-    
+
+    document.getElementById("clock1").innerText = washingtonTime;
+    document.getElementById("clock1").textContent = washingtonTime;
+
     setTimeout(washingtonTimeClock, 1000);
-    
 }
 washingtonTimeClock();
+
+function sanJuanTimeClock(){
+    var sanJuanTime = new Date().toLocaleString("en-US", {timeZone: "America/Caracas", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock2").innerText = sanJuanTime;
+    document.getElementById("clock2").textContent = sanJuanTime;
+
+    setTimeout(sanJuanTimeClock, 1000);
+}
+sanJuanTimeClock();
+
+function buenosAiresTimeClock(){
+    var buenosAiresTime = new Date().toLocaleString("en-US", {timeZone: "America/Argentina/Buenos_Aires", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock3").innerText = buenosAiresTime;
+    document.getElementById("clock3").textContent = buenosAiresTime;
+
+    setTimeout(buenosAiresTimeClock, 1000);
+}
+buenosAiresTimeClock();
+
+function kingEdwardPointTimeClock(){
+    var kingEdwardPointTime = new Date().toLocaleString("en-US", {timeZone: "Etc/GMT-2", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock4").innerText = kingEdwardPointTime;
+    document.getElementById("clock4").textContent = kingEdwardPointTime;
+
+    setTimeout(kingEdwardPointTimeClock, 1000);
+}
+kingEdwardPointTimeClock();
+
+function capeVerdeTimeClock(){
+    var capeVerdeTime = new Date().toLocaleString("en-US", {timeZone: "Atlantic/Cape_Verde", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock5").innerText = capeVerdeTime;
+    document.getElementById("clock5").textContent = capeVerdeTime;
+
+    setTimeout(capeVerdeTimeClock, 1000);
+}
+capeVerdeTimeClock();
+
+function zuluTimeClock(){
+    var zuluTime = new Date().toLocaleString("en-US", {timeZone: "Zulu", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock6").innerText = zuluTime;
+    document.getElementById("clock6").textContent = zuluTime;
+
+    setTimeout(zuluTimeClock, 1000);
+}
+zuluTimeClock();
+
+function londonTimeClock(){
+    var londonTime = new Date().toLocaleString("en-US", {timeZone: "Europe/London", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock7").innerText = londonTime;
+    document.getElementById("clock7").textContent = londonTime;
+
+    setTimeout(londonTimeClock, 1000);
+}
+londonTimeClock();
 
 function euCommandTimeClock(){
     var euCommandTime = new Date().toLocaleString("en-US", {timeZone: "Europe/Berlin", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("euCommandClock").innerText = euCommandTime;
-    document.getElementById("euCommandClock").textContent = euCommandTime;
+    document.getElementById("clock8").innerText = euCommandTime;
+    document.getElementById("clock8").textContent = euCommandTime;
 
     setTimeout(euCommandTimeClock, 1000);
 }
 euCommandTimeClock();
 
+function bucharestTimeClock(){
+    var bucharestTime = new Date().toLocaleString("en-US", {timeZone: "Europe/Bucharest", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock9").innerText = bucharestTime;
+    document.getElementById("clock9").textContent = bucharestTime;
+
+    setTimeout(bucharestTimeClock, 1000);
+}
+bucharestTimeClock();
+
+function moscowTimeClock(){
+    var moscowTime = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock10").innerText = moscowTime;
+    document.getElementById("clock10").textContent = moscowTime;
+
+    setTimeout(moscowTimeClock, 1000);
+}
+moscowTimeClock();
+
 function tehranTimeClock(){
     var tehranTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Tehran", timeStyle: "medium", hourCycle: "h23"});
-    
-    document.getElementById("tehranClock").innerText = tehranTime;
-    document.getElementById("tehranClock").textContent = tehranTime;
+
+    document.getElementById("clock11").innerText = tehranTime;
+    document.getElementById("clock11").textContent = tehranTime;
 
     setTimeout(tehranTimeClock, 1000);
 }
 tehranTimeClock();
 
+function dubaiTimeClock(){
+    var dubaiTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Dubai", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock12").innerText = dubaiTime;
+    document.getElementById("clock12").textContent = dubaiTime;
+
+    setTimeout(dubaiTimeClock, 1000);
+}
+dubaiTimeClock();
+
 function kabulTimeClock(){
     var kabulTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Kabul", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("kabulClock").innerText = kabulTime;
-    document.getElementById("kabulClock").textContent = kabulTime;
+    document.getElementById("clock13").innerText = kabulTime;
+    document.getElementById("clock13").textContent = kabulTime;
 
     setTimeout(kabulTimeClock, 1000);
 }
 kabulTimeClock();
 
+function islamabadTimeClock(){
+    var islamabadTime = new Date().toLocaleString("en-US", {timeZone: "Indian/Maldives", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock14").innerText = islamabadTime;
+    document.getElementById("clock14").textContent = islamabadTime;
+
+    setTimeout(islamabadTimeClock, 1000);
+}
+islamabadTimeClock();
+
+function kolkataTimeClock(){
+    var kolkataTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock15").innerText = kolkataTime;
+    document.getElementById("clock15").textContent = kolkataTime;
+
+    setTimeout(kolkataTimeClock, 1000);
+}
+kolkataTimeClock();
+
+function kathmanduTimeClock(){
+    var kathmanduTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Kathmandu", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock16").innerText = kathmanduTime;
+    document.getElementById("clock16").textContent = kathmanduTime;
+
+    setTimeout(kathmanduTimeClock, 1000);
+}
+kathmanduTimeClock();
+
+function dhakaTimeClock(){
+    var dhakaTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Dhaka", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock17").innerText = dhakaTime;
+    document.getElementById("clock17").textContent = dhakaTime;
+
+    setTimeout(dhakaTimeClock, 1000);
+}
+dhakaTimeClock();
+
+function jakartaTimeClock(){
+    var jakartaTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Jakarta", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock18").innerText = jakartaTime;
+    document.getElementById("clock18").textContent = jakartaTime;
+
+    setTimeout(jakartaTimeClock, 1000);
+}
+jakartaTimeClock();
+
 function beijingTimeClock(){
     var beijingTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("beijingClock").innerText = beijingTime;
-    document.getElementById("beijingClock").textContent = beijingTime;
+    document.getElementById("clock19").innerText = beijingTime;
+    document.getElementById("clock19").textContent = beijingTime;
 
     setTimeout(beijingTimeClock, 1000);
 }
@@ -52,29 +191,129 @@ beijingTimeClock();
 function tokyoTimeClock(){
     var tokyoTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Tokyo", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("tokyoClock").innerText = tokyoTime;
-    document.getElementById("tokyoClock").textContent = tokyoTime;
+    document.getElementById("clock20").innerText = tokyoTime;
+    document.getElementById("clock20").textContent = tokyoTime;
 
     setTimeout(tokyoTimeClock, 1000);
 }
 tokyoTimeClock();
 
-function zuluTimeClock(){
-    var zuluTime = new Date().toLocaleString("en-US", {timeZone: "Pacific/Midway", timeStyle: "medium", hourCycle: "h23"});
+function aliceSpringsTimeClock(){
+    var aliceSpringsTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Darwin", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("zuluClock").innerText = zuluTime;
-    document.getElementById("zuluClock").textContent = zuluTime;
+    document.getElementById("clock21").innerText = aliceSpringsTime;
+    document.getElementById("clock21").textContent = aliceSpringsTime;
 
-    setTimeout(zuluTimeClock, 1000);
+    setTimeout(aliceSpringsTimeClock, 1000);
 }
-zuluTimeClock();
+aliceSpringsTimeClock();
+
+function brisbaneTimeClock(){
+    var brisbaneTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock22").innerText = brisbaneTime;
+    document.getElementById("clock22").textContent = brisbaneTime;
+
+    setTimeout(brisbaneTimeClock, 1000);
+}
+brisbaneTimeClock();
+
+function adelaideTimeClock(){
+    var adelaideTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Adelaide", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock23").innerText = adelaideTime;
+    document.getElementById("clock23").textContent = adelaideTime;
+
+    setTimeout(adelaideTimeClock, 1000);
+}
+adelaideTimeClock();
+
+function sydneyTimeClock(){
+    var sydneyTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Sydney", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock24").innerText = sydneyTime;
+    document.getElementById("clock24").textContent = sydneyTime;
+
+    setTimeout(sydneyTimeClock, 1000);
+}
+sydneyTimeClock();
+
+function wakeIslandTimeClock(){
+    var wakeIslandTime = new Date().toLocaleString("en-US", {timeZone: "Pacific/Wake", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock25").innerText = wakeIslandTime;
+    document.getElementById("clock25").textContent = wakeIslandTime;
+
+    setTimeout(wakeIslandTimeClock, 1000);
+}
+wakeIslandTimeClock();
+
+function bakerIslandTimeClock(){
+    var bakerIslandTime = new Date().toLocaleString("en-US", {timeZone: "Etc/GMT-12", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock26").innerText = bakerIslandTime;
+    document.getElementById("clock26").textContent = bakerIslandTime;
+
+    setTimeout(bakerIslandTimeClock, 1000);
+}
+bakerIslandTimeClock();
+
+function midwayTimeClock(){
+    var midwayTime = new Date().toLocaleString("en-US", {timeZone: "Pacific/Midway", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock27").innerText = midwayTime;
+    document.getElementById("clock27").textContent = midwayTime;
+
+    setTimeout(midwayTimeClock, 1000);
+}
+midwayTimeClock();
+
+function honoluluTimeClock(){
+    var honoluluTime = new Date().toLocaleString("en-US", {timeZone: "Pacific/Honolulu", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock28").innerText = honoluluTime;
+    document.getElementById("clock28").textContent = honoluluTime;
+
+    setTimeout(honoluluTimeClock, 1000);
+}
+honoluluTimeClock();
+
+function gambierTimeClock(){
+    var gambierTime = new Date().toLocaleString("en-US", {timeZone: "Pacific/Gambier", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock29").innerText = gambierTime;
+    document.getElementById("clock29").textContent = gambierTime;
+
+    setTimeout(gambierTimeClock, 1000);
+}
+gambierTimeClock();
 
 function pacificTimeClock(){
     var pacificTime = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles", timeStyle: "medium", hourCycle: "h23"});
 
-    document.getElementById("pacificClock").innerText = pacificTime;
-    document.getElementById("pacificClock").textContent = pacificTime;
+    document.getElementById("clock30").innerText = pacificTime;
+    document.getElementById("clock30").textContent = pacificTime;
 
     setTimeout(pacificTimeClock, 1000);
 }
 pacificTimeClock();
+
+function mountainTimeClock(){
+    var mountainTime = new Date().toLocaleString("en-US", {timeZone: "America/Denver", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock31").innerText = mountainTime;
+    document.getElementById("clock31").textContent = mountainTime;
+
+    setTimeout(mountainTimeClock, 1000);
+}
+mountainTimeClock();
+
+function centralTimeClock(){
+    var centralTime = new Date().toLocaleString("en-US", {timeZone: "America/Chicago", timeStyle: "medium", hourCycle: "h23"});
+
+    document.getElementById("clock32").innerText = centralTime;
+    document.getElementById("clock32").textContent = centralTime;
+
+    setTimeout(centralTimeClock, 1000);
+}
+centralTimeClock();
